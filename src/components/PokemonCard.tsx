4250c5303a44f4ebab5a8f040ac8af524a4e3db3
Salomao Souza <souza95salomao@gmail.com>
2021-03-10 13:49:10 -0400

    [UPDATE] Pokedex was finnaly populated but still buggy

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -6,7 +6,7 @@ export function PokemonCard(props) {
     return (
         <div className={styles.cardContainer}>
             <p className={styles.pokename}>{props.name}</p>
-            <img className={styles.pokepic} src={props.url} alt=""/>
+            <img className={styles.pokepic} src={props.url} alt="Pokemon picture"/>
             <div className={styles.types}>
                 <p>{props.type1}</p>
                 <p>{props.type2}</p>
--- src/components/PokemonContainer.tsx
+++ src/components/PokemonContainer.tsx
@@ -1,11 +1,60 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import { PokemonCard } from './PokemonCard';
 import { getPokemons, getPokemonData } from './Pokemon';
 import styles from '../styles/components/PokemonContainer.module.css';
 
 export default function PokemonContainer() {
+    const [dex, setDex] = useState([]) 
+    const [pokedex, setPokedex] = useState([]) 
+
+    const populateDex = () => {
+        dex.forEach(mon => {
+            console.log(`{ 
+                ${mon.name} 
+                ${mon.pic}
+                ${mon.types[0].type.name}
+                ${mon.types[1]?.type.name}
+            }`)
+        })
+     }
+
+    useEffect(()=>{
+        getPokemons().then(res=>{
+            setPokedex(res)
+        })
+    }, [])
+
+    useEffect(()=>{
+        let dex = []
+        pokedex.map(pokemon=>{
+            getPokemonData(pokemon.url).then(res=>{
+                const {id, name, sprites, types} = res
+                dex.push({
+                    id: id,
+                    name: name,
+                    pic: sprites.front_default,
+                    type1: types[0].type.name,
+                    type2: types[1]?.type.name
+                })
+            })
+        })
+        setDex(dex)
+    }, [pokedex])
+
     return (
         <div className={styles.container}>
+            {
+                dex.length>0&&(dex.map(poke=>{
+                    return (
+                        <PokemonCard name={poke.name} 
+                                     url={poke.pic}
+                                     type1={poke.type1}
+                                     type2={poke.type2}
+                                     key={poke.id}
+                        />
+                    )
+                }))
+            }
         </div>
     )
 }
