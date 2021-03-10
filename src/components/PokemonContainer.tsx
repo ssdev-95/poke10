ab62ddd0acb2579eb773d95f03d6ae0b655a8f23
Salomao Souza <souza95salomao@gmail.com>
2021-03-10 16:57:05 -0400

    [FIX] Not at least one PokemonCard was visible in PokemonContainer

--- src/components/PokemonContainer.tsx
+++ src/components/PokemonContainer.tsx
@@ -5,20 +5,27 @@ import styles from '../styles/components/PokemonContainer.module.css';
 
 export default function PokemonContainer() {
     const [dex, setDex] = useState([]) 
-    const [pokedex, setPokedex] = useState([]) 
-
-    /*const populateDex = () => {
-        dex.forEach(mon => {
-            console.log(`{ 
-                ${mon.name} 
-                ${mon.pic}
-                ${mon.types[0].type.name}
-                ${mon.types[1]?.type.name}
-            }`)
-        })
-     }*/
+    //const [pokedex, setPokedex] = useState([]) 
 
     useEffect(()=>{
+        getPokemons().then(res=>{
+            res.map(pokemon=>{
+                getPokemonData(pokemon.url).then(ponse=>{
+                    const {id, name, sprites, types} = ponse
+                    console.log(ponse)
+                    setDex([...dex, {
+                        id: id,
+                        name: name,
+                        pic: sprites.front_default,
+                        type1: types[0].type.name,
+                        type2: types[1]?.type.name
+                    }])
+                })
+            })
+        })
+    }, [])    
+
+    /*useEffect(()=>{
         getPokemons().then(res=>{
             setPokedex(res)
         })
@@ -37,19 +44,36 @@ export default function PokemonContainer() {
                 }])
             })
         })
-    }, [dex])
+    }, [dex])*/
+
+    /*function populate() {
+        dex.length>0&&(dex.map(poke=>{
+            return (
+                    <PokemonCard name={poke.name} 
+                                 url={poke.pic}
+                                 type1={poke.type1}
+                                 type2={poke.type2}
+                                 key={poke.id}
+                    />
+            )
+        }))
+    }*/
+
+    useEffect(() => {
+        console.log(dex)
+    }, [])
 
     return (
         <div className={styles.container}>
             {
                 dex.length>0&&(dex.map(poke=>{
                     return (
-                        <PokemonCard name={poke.name} 
-                                     url={poke.pic}
-                                     type1={poke.type1}
-                                     type2={poke.type2}
-                                     key={poke.id}
-                        />
+                            <PokemonCard name={poke.name} 
+                                         url={poke.pic}
+                                         type1={poke.type1}
+                                         type2={poke.type2}
+                                         key={poke.id}
+                            />
                     )
                 }))
             }
