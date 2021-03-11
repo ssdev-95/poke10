ac0bc8e0e335d5c05c3e53e3d6b665b08c1ad8fb
Salomao Souza <souza95salomao@gmail.com>
2021-03-11 12:42:40 -0400

    [FIX] Dex items not rendering properly on pokedex container

--- src/components/PokemonContainer.tsx
+++ src/components/PokemonContainer.tsx
@@ -1,82 +1,43 @@
 import React, {useEffect, useState} from 'react'
-import { PokemonCard } from './PokemonCard';
 import { getPokemons, getPokemonData } from './Pokemon';
+import { PokemonCard } from './PokemonCard';
+
 import styles from '../styles/components/PokemonContainer.module.css';
 
 export default function PokemonContainer() {
-    const [dex, setDex] = useState([]) 
-    //const [pokedex, setPokedex] = useState([]) 
+    const [dex, setDex] = useState([])
+    const [pokedex, setPokedex] = useState([])
 
     useEffect(()=>{
         getPokemons().then(res=>{
-            res.map(pokemon=>{
-                getPokemonData(pokemon.url).then(ponse=>{
-                    const {id, name, sprites, types} = ponse
-                    console.log(ponse)
-                    setDex([...dex, {
-                        id: id,
-                        name: name,
-                        pic: sprites.front_default,
-                        type1: types[0].type.name,
-                        type2: types[1]?.type.name
-                    }])
+            res.map(result=>{
+                getPokemonData(result.url).then(ponse=>{
+                    const {id, name, types, sprites} = ponse
+                    setDex([...dex, {pokeid: id, pokename: name,
+                                     poketype1: types[0].type.name,
+                                     poketype2: types[1]?.type.name,
+                                     pokepic: sprites.front_default}])
                 })
             })
         })
-    }, [])    
-
-    /*useEffect(()=>{
-        getPokemons().then(res=>{
-            setPokedex(res)
-        })
-    }, [])
+    },[])
 
     useEffect(()=>{
-        pokedex.map(pokemon=>{
-            getPokemonData(pokemon.url).then(res=>{
-                const {id, name, sprites, types} = res
-                setDex([...dex, {
-                    id: id,
-                    name: name,
-                    pic: sprites.front_default,
-                    type1: types[0].type.name,
-                    type2: types[1]?.type.name
-                }])
-            })
-        })
-    }, [dex])*/
+        try{dex.map(poke=>{
+            setPokedex([...pokedex, <PokemonCard key={poke.pokeid} name={`${poke.pokeid}. ${poke.pokename}`}
+                                                 type1={poke.poketype1}
+                                                 type2={poke.poketype2}
+                                                 url={poke.pokepic}/>])
+        })}catch(err) {
+            console.log(err)
+        }
+        console.log(dex.length)
 
-    /*function populate() {
-        dex.length>0&&(dex.map(poke=>{
-            return (
-                    <PokemonCard name={poke.name} 
-                                 url={poke.pic}
-                                 type1={poke.type1}
-                                 type2={poke.type2}
-                                 key={poke.id}
-                    />
-            )
-        }))
-    }*/
-
-    useEffect(() => {
-        console.log(dex)
-    }, [])
+    }, [dex])
 
     return (
         <div className={styles.container}>
-            {
-                dex.length>0&&(dex.map(poke=>{
-                    return (
-                            <PokemonCard name={poke.name} 
-                                         url={poke.pic}
-                                         type1={poke.type1}
-                                         type2={poke.type2}
-                                         key={poke.id}
-                            />
-                    )
-                }))
-            }
+            {pokedex}
         </div>
     )
 }
